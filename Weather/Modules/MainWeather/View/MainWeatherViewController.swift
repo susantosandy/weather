de6c471cffaf00d5c09ec457c3fb85a906c50d91
Sandy Susanto <sandy.susanto.dev@outlook.com>
2021-04-13 21:01:45 +0700

    Add View Main Weather

--- Weather/Modules/MainWeather/View/MainWeatherViewController.swift
+++ Weather/Modules/MainWeather/View/MainWeatherViewController.swift
@@ -9,12 +9,27 @@ import UIKit
 
 class MainWeatherViewController: BaseViewController {
     
+    @IBOutlet weak var buttonCurrentLocation: UIButton!
+    @IBOutlet weak var buttonSearch: UIButton!
+    @IBOutlet weak var buttonOptionMenu: UIButton!
+    
     var presenter: MainWeatherViewToPresenterProtocol!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func currentLocationPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func searchLocationPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func optionMenuPressed(_ sender: UIButton) {
+    }
+    
 }
 
 extension MainWeatherViewController: MainWeatherPresenterToViewProtocol {
