900d3e3d45953d910c96a7245eaefd3837425a56
Sandy Susanto <sandy.susanto.dev@outlook.com>
2021-04-13 18:49:02 +0700

    Init Project

---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Sandy Susanto on 13/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
